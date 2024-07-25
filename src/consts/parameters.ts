8f4affe4f1f80312be05a50c5c3d18338fc42a6e
amlovers <amlo.onchain@gmail.com>
2024-07-25 12:56:07 -0600

    editInicial

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,17 +2,17 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0xBaD843407d94B2C17f78D6718f1F8fb7bE8C9fB5";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
-export const chainConst = "mumbai";
+export const chainConst = "base";
 
 // It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "c18c0526e9331130bca42825f93bca03";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
